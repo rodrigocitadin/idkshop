ec096d97742884c31b2e2498b1adcccc43b2ef68
rodrigocitadin <citadinrodrigo@gmail.com>
2024-07-06 20:57:16 -0300

    wip wip wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,30 @@
+import Image from "next/image"
+
+import haskell from "@/shirts/haskell.png"
+import ocamel from "@/shirts/ocamel.png"
+import rust from "@/shirts/rust.png"
+
 export default function Home() {
+  const shirts = [
+    { img: haskell, id: 1, width: 450, height: 500 },
+    { img: ocamel, id: 2, width: 450, height: 500 },
+    { img: rust, id: 3, width: 450, height: 500 },
+  ]
+
   return (
-    <main>WIP</main>
+    <div className="flex gap-16">
+      {
+        shirts.map(shirt => (
+          <div className="min-w-[600px] min-h-[600px] bg-stone-600 flex flex-col justify-center items-center rounded-3xl">
+            <Image
+              src={shirt.img}
+              width={shirt.width}
+              height={shirt.height}
+              alt=""
+            />
+          </div>
+        ))
+      }
+    </div>
   );
 }
