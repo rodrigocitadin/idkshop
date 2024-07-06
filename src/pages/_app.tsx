736ca5f2457946285c911f19b2663289b771d561
rodrigocitadin <citadinrodrigo@gmail.com>
2024-07-06 20:49:17 -0300

    wip

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-// import "@/styles/globals.css";
+import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Roboto } from "next/font/google";
 
@@ -6,7 +6,12 @@ const roboto = Roboto({ subsets: ["latin"], weight: ["400", "700"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={roboto.className}>
+    <main className={`flex flex-col justify-center items-start bg-stone-900 min-h-screen text-stone-100 ${roboto.className}`}>
+      <div className="max-w-[1180px] w-full mx-auto py-2">
+        <strong className="text-4xl text-amber-600">
+          Idkshop.
+        </strong>
+      </div>
       <Component {...pageProps} />
     </main>
   )
